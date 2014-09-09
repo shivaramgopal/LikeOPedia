812f3e636e18fc24048f534e2dd40045651f779a
shivaramgopal <sg3681@nyu.edu>
2014-09-08 22:05:49 -0400

    Sign up form to create a page or just as user

---
+++ form.php
@@ -0,0 +1,23 @@
+<title>
+Sign up
+</title>
+<head>
+
+<link rel="stylesheet" type="text/css" href="style.css" />
+<title>Search results</title>
+
+</head>
+  <div id="main">
+  <h1><img src="ostuck.jpg" width="140" height="37"/></b></h1>
+  		<div id="header">
+	
+    	
+ 
+		Sign up and start rating stuff! 
+    <h6></h6>
+    </div><br><br>
+<font color="white"><li><a href="member.php"><font size="3" style="font-family:tahoma" > <b>I want to Sign up as a user.</li></b></a>
+<font color="gray" size="2">(sign up as just a user & start liking/disliking stuff.)
+<br><br><br>
+<font color="white"><li><a href="page.php"><font size="3" style="font-family:tahoma"><b>I want to Sign up as a user & create a page for my product.</b></li></a>
+<font color="gray" size="2">(sign up as a user and create page for your product/stuff.)
